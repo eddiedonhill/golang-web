7aab016cbbd631793acbd67b1979bed4b5bbde00
eddiehilll <edonaldhill@gmail.com>
2017-05-20 00:35:12 -0700

    Create sunday.go

---
+++ building-sites/sunday.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+        "net/http"
+        "io"
+        "fmt"
+)
+
+func main() {
+        http.HandleFunc("/", index)
+        http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="images/heat.jpg">	`)
+}
+
+func heatPic (w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "heat.jpg")
+}
