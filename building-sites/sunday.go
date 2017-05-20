e101e13376108eca30d7608bdc2f7e7b4ce896d7
eddiehilll <edonaldhill@gmail.com>
2017-05-20 00:45:23 -0700

    Update sunday.go

--- building-sites/sunday.go
+++ building-sites/sunday.go
@@ -13,7 +13,7 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="images/heat.jpg">	`)
+	io.WriteString(w, `<img src="images/heat.jpg">	"Favorite Movie: Heat" `)
 }
 
 func heatPic (w http.ResponseWriter, req *http.Request) {
