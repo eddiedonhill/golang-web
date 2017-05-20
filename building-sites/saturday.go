723b657f49992d0a6395f6dad51f86cb598e3d7e
eddiehilll <edonaldhill@gmail.com>
2017-05-20 00:44:56 -0700

    Update saturday.go

--- building-sites/saturday.go
+++ building-sites/saturday.go
@@ -13,7 +13,7 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="images/cheetah.jpg">	`)
+	io.WriteString(w, `<img src="images/cheetah.jpg"> "Favorite Animal: Cheetah" `)
 }
 
 func cheetahPic (w http.ResponseWriter, req *http.Request) {
